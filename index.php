c5a22cec388b9f08b2f0079b820f4f3824e2dfeb
Lanes Rocha <lanes.rocha@hotmail.com>
2018-12-03 09:33:49 -0200

    criei meu repositorio

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>CURSO DE PHP UDEMY</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php 
+		echo"Olá mundo!!!";
+	?>
+</body>
+</html>
