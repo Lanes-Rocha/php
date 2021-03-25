ff4cff35f9382aa03a41e7d668c3b93493b891ab
Lanes Rocha <lanes.rocha@hotmail.com>
2021-03-24 23:23:07 -0300

    Atualizado
    ...

--- classe-date-time.php
+++ classe-date-time.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Trabalando com a Classe DateTime()</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-    </head>
-    <body>
-    
-    </body>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Trabalhando com o DateTime()</title>
+	<link rel="stylesheet" href="">
+</head>
+	<body>
+		<?php 
+			$dt = new DateTime();
+			$dt->add();
+			
+			echo $dt->format("d/m/Y H:i:s");
+
+		 ?>
+	</body>
 </html>
