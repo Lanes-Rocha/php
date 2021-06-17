51775c80b1e7d9577c61f903c0ab2a04d03d2921
Lanes Rocha <lanes.rocha@hotmail.com>
2021-06-17 02:18:30 -0300

    Finalizado o conteúdo sobre autoload

--- AUTOLOAD/exemplo01.php
+++ AUTOLOAD/exemplo01.php
@@ -1,4 +1,7 @@
 <?php
+//Este códio não irá funcionar pois o arquivo Automovel.php está dentro da pasta Abstrata
+// Neste caso para colocar o código funcional basta colocar o arquivo na raiz da pasta principal AUTOLOAD.
+
 function __autoload($nomeClasse){
     var_dump($nomeClasse);// O var_dump neste caso mostra a chamado das duas classes 
 
---
+++ AUTOLOAD/exemplo02.php
@@ -0,0 +1,17 @@
+<?php
+    function incluirClasses($nomeClasse){
+        if(file_exists($nomeClasse.".php") === true){
+            require_once($nomeClasse.".php");
+        }
+    }
+    //spl siginifica Standard PHP Librire
+    spl_autoload_register("incluirClasses");
+    spl_autoload_register(function($nomeClasse){
+        if(file_exists("Abstrata" . DIRECTORY_SEPARATOR . $nomeClasse.".php") === true){
+            require_once("Abstrata" . DIRECTORY_SEPARATOR . $nomeClasse.".php");
+        }
+    });
+
+    $carro = new DelRey();
+    $carro->acelerar(80);
+?>
