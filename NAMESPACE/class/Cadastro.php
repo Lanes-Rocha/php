97e004070d6968e0b8b2e6ff0209a1a1f62d31b4
Lanes Rocha <lanes.rocha@hotmail.com>
2021-06-21 20:34:35 -0300

    criação dos gets e sets

--- NAMESPACE/class/Cadastro.php
+++ NAMESPACE/class/Cadastro.php
@@ -3,7 +3,7 @@
         private $nome;
         private $email;
         private $senha;
-
+        // criando os gets
         public function getNome():string{
             return $this->nome;
         }
@@ -12,6 +12,16 @@
         }
         public function getSenha():string{
             return $this->senha;
+        }
+        // criando os sets
+        public function setNome($nome){
+            return $this->nome = $nome;
+        }
+        public function setNome($email){
+            return $this->email = $email;
+        }
+        public function setNome($senha){
+            return $this->senha = $senha;
         }
     }
 ?>
