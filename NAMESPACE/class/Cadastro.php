0e5ff48cf085650f92ff3ab9d8b7c50e3c634839
Lanes Rocha <lanes.rocha@hotmail.com>
2021-06-21 20:31:34 -0300

    Criação da classe Cadastro.php

---
+++ NAMESPACE/class/Cadastro.php
@@ -0,0 +1,17 @@
+<?php
+    class Cadastro{
+        private $nome;
+        private $email;
+        private $senha;
+
+        public function getNome():string{
+            return $this->nome;
+        }
+        public function getEmail():string{
+            return $this->email;
+        }
+        public function getSenha():string{
+            return $this->senha;
+        }
+    }
+?>
---
+++ NAMESPACE/config.php
@@ -0,0 +1,12 @@
+<?php
+    spl_autoload_register(function($nameClasse){
+        $dirClasse = "class";
+        $filename = $dirClasse . DIRECTORY_SEPARATOR . $nameClasse . ".php";
+
+        if(file_exists($filename)){
+            require_once($filename);
+        }
+
+    });
+
+?>
