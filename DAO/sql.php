ec62901b243807cbfe648b7d661931def8f6247b
Lanes Rocha <lanes.rocha@hotmail.com>
2021-08-21 12:11:04 -0300

    atualização do arquivo Sql.php

---
+++ DAO/sql.php
@@ -0,0 +1,27 @@
+<?php
+    class Sql extends PDO{
+        private $conn;
+    
+        public function __construct(){
+            $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
+        }
+        private function setParams($statment, $parameters = array()){
+            
+        }
+
+        public function query($rawQuery, $params = array()){
+
+            $stmt = $this->conn->prepare($rawQuery);
+
+            foreach($params as $key => $value){
+
+                $stmt->bindParam($key, $value);
+
+            }
+        }
+        
+
+
+
+    }
+?>
